18d0c2df0bea3866703e2dac7038975c053f1517
nikrod <xniko14@hotmail.com>
2014-11-16 21:28:05 -0300

    arreglo

--- tarea2.cpp
+++ tarea2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include <fstream>
 #define 	MAXLINEA 	225
 
@@ -9,31 +8,7 @@ typedef char 	str_linea[255];
 
 using namespace std;
 
-void buscarR(StringN nombre)
-{
-    char *token;
-    char *token2;
-	cout<<"Leyendo Archivo:"<<endl;	
-	char *linea;
-	FILE		*pArchivo;
-	pArchivo=fopen("chilenos.csv","rt");
-    
-	while(!feof(pArchivo))
-	{
-	    fgets(linea,MAXLINEA,pArchivo);
-        token = strtok(linea,"|");
-        token2=token;
-	    cout<<token<<endl;
-        token = strtok(NULL,"|");
-	    /**if(strcmp(nombre,token)==0)
-	    {
-            cout<<token2;                       
-        }**/
-                                   
-	}   
- }
-
-void buscarN (StringN nombre)
+void buscarR (StringN rut2)
 {
     StringN nombre2;
     char *token;
@@ -46,35 +21,231 @@ void buscarN (StringN nombre)
     int x=0;
    	str_linea   linea;
 	FILE		*pArchivo;
-	
-	cout<<"Leyendo Archivo:"<<endl;	
 	pArchivo=fopen("chilenos.csv","rt");
 	while(!feof(pArchivo) && x==0)
 	{
         fgets(linea,MAXLINEA,pArchivo);
         rut= strtok(linea,"|");
-        token = strtok(NULL,"|");	
-        if(strcmp(nombre,token)==0)
+        token= strtok(NULL,"|");
+        cam1 = strtok(NULL,"|");
+        cam2 = strtok(NULL,"|");
+        cam3 = strtok(NULL,"|");
+        cam4 = strtok(NULL,"|");
+        cam5 = strtok(NULL,"|");	
+        if(strcmp(rut2,rut)==0)
         {
-           cout<<"si"<<endl;
-           cout<<"Rut:"<<rut<<endl;
-           cout<<"Nombre:"<<token<<endl;
+            if(rut==NULL)
+                 {
+                     cout<<"rut: no se tiene registro"<<endl;
+                 }
+               else
+                 {
+                     cout<<"Rut: "<<rut<<endl;
+                 }
+               if(token==NULL)
+                   {
+                      cout<<"nombre: no se tiene registro"<<endl;
+                   }
+               else
+                   {
+                       cout<<"Nombre: "<<token<<endl;
+                   }
+               if(cam1==NULL)
+                   {
+                       cout<<"sexo: no se tiene registro"<<endl;
+                   }
+               else
+                   {
+                       cout<<"sexo: "<<cam1<<endl; 
+                   }
+              if(cam2==NULL)
+                   {
+                       cout<<"campo2: no se tiene registro"<<endl;
+                   }
+               else
+                   {
+                       cout<<"direccion: "<<cam2<<endl;           
+                   }             
+              if(cam3==NULL)
+                   {
+                       cout<<"campo3: no se tiene registro"<<endl;  
+                   }
+              else
+                   {
+                       cout<<"direccion: "<<cam3<<endl;
+                   }
+              if(cam4==NULL)
+                   {
+                       cout<<"campo4: no se tiene registro"<<endl;
+                   }
+              else
+                   {
+                       cout<<"direccion: "<<cam4<<endl;
+                   }    
+              if(cam5==NULL)
+                   {
+                       cout<<"campo5: no se tiene registro"<<endl;
+                   }
+              else
+                   {
+                       cout<<"direccion: "<<cam5<<endl;
+                   }
+           
            x=1;	
            
         }
+        
 	}
+	if(x==0)
+	{
+        cout<<"no se encontraron registros"<<endl;
+    }   
+}
+
+void buscarN (StringN nombre, int largo)
+{
+    StringN nombre2;
+    int aceptar=0;
+    char *token;
+    char *rut;
+    char *cam1;
+    char *cam2;
+    char *cam3;
+    char *cam4;
+    char *cam5;
+    char *nom;
+    char *app;
+    int x=0;
+   	str_linea   linea;
+	FILE		*pArchivo;
+	pArchivo=fopen("chilenos.csv","rt");
+
+        while((fgets(linea,MAXLINEA,pArchivo)!= NULL) && x==0)
+        {
+        rut= strtok(linea,"|");//se corta el rut
+        
+        token= strtok(NULL,"|");//se corta el nombre
+       
+        cam1 = strtok(NULL,"|");//campo1,2,3,4,5..
+        
+        cam2 = strtok(NULL,"|");
+        
+        cam3 = strtok(NULL,"|");
+        
+        cam4 = strtok(NULL,"|");
+       
+        cam5 = strtok(NULL,"|");	
+        
+        for(int i=0;i<largo;i++)
+        {
+                if(token[i]==nombre[i])
+                {
+                    aceptar=1;
+                }
+                else
+                {
+                    aceptar=0;
+                    i=largo;
+                }
+        }
+        if (aceptar==1)
+        {        
+               if(rut==NULL)
+                   {
+                     cout<<"no se tiene registro"<<endl;
+                   }
+               else
+                   {
+                     cout<<"Rut: "<<rut<<endl;
+                   }
+               if(token=="")
+                   {
+                     cout<<"nombre: no se tiene registro"<<endl;
+                   }
+               else
+                   {
+                     cout<<"Nombre:"<<token<<endl;
+                   }
+               if(cam1==NULL)
+                   {
+                     cout<<"sexo: no se tiene registro"<<endl;
+                   }
+               else
+                   {
+                     cout<<"sexo: "<<cam1<<endl; 
+                   }
+              if(cam2==NULL)
+                   {
+                     cout<<"campo2: no se tiene registro"<<endl;
+                   }
+              else
+                   {
+                     cout<<"direccion: "<<cam2<<endl;           
+                   }             
+              if(cam3==NULL)
+                   {
+                     cout<<"campo3: no se tiene registro"<<endl;  
+                   }
+              else
+                   {
+                     cout<<"direccion: "<<cam3<<endl;
+                   }
+              if(cam4==NULL)
+                   {
+                     cout<<"campo4: no se tiene registro"<<endl;
+                   }
+              else
+                   {
+                    cout<<"direccion: "<<cam4<<endl;
+                   }    
+              if(cam5==NULL)
+                   {
+                    cout<<"campo5: no se tiene registro"<<endl;
+                   }
+              else
+                   {
+                    cout<<"direccion: "<<cam5<<endl;
+                   }
+           x=1;	
+       }
+       
+       
+   
+	}
+	if(x==0)
+            {
+            cout<<"no se tienen registros"<<endl;
+	        fclose (pArchivo);
+            }
+    
 }
 
 int main(){	
-    
+    int op;
     StringN nombre;
-    cout<<"ingrese nombre"<<endl;
-    cin>>nombre;
-    buscarN(nombre);
+    StringN rut;
+    int j=1;
+    while (j!=0)
+    {
+          cout<<"1-buscar por nombre"<<endl;
+          cout<<"2-buscar por rut"<<endl;
+          cin>>op;
+          if(op==1)
+          {
+             cout<<"ingrese nombre"<<endl;
+             fgets(nombre,40,stdin);
+             scanf("%[^\n]", nombre);
+             buscarN(nombre,strlen(nombre));
+             j=0;
+          }
+          if(op==2)
+          {
+             cout<<"ingrese rut"<<endl;
+             cin>>rut;
+             buscarR(rut);
+             j=0;
+          }            
+    }      
 	system("pause");
 }
-
-
-
-
 
